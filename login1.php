55c596994473268436b0c6515e2fcc2a38f154df
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-09-17 18:36:46 +0800

    kl

--- login1.php
+++ login1.php
@@ -573,7 +573,7 @@ h3 {
 
             <div class="form-group">
 							<label>Password</label>
-							<input type="text" name="password" class="form-control" required="required"/>
+							<input type="password" name="password" class="form-control" required="required"/>
 						</div>
 						
 						<div class="form-group">
@@ -645,7 +645,7 @@ h3 {
 
             <div class="form-group">
 							<label>Change Password</label>
-							<input type="text" name="password" class="form-control" required="required"/>
+							<input type="password" name="password" class="form-control" required="required"/>
 						</div>
 						
 						
