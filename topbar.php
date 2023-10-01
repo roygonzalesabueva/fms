7d9994d6bfa6f618800af625b90e5b56135b2da5
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-01 19:49:03 +0800

    Update topbar.php

--- topbar.php
+++ topbar.php
@@ -45,17 +45,17 @@
   <div class="container-fluid mt-1 mb-1">
   	
   <img src="davsur2.png" width="220" height="60">
-			  <h3><font color="WHITE"><B>Memorandum in Electronic Media Online (M.E.M.O)</B></font></h3>
+			  <h5><font color="WHITE"><B>Memorandum in Electronic Media Online (M.E.M.O)</B></font></h5>
 
 
 
         
 
   			
-        <font color="White" size="4"><B><a <p id="demo"></p></a></B></font> 
+        <font color="White" size="2"><B><a <p id="demo"></p></a></B></font> 
 
-        <a href="#"><font color="WHITE " size="4"><B><?=$_SESSION['username']?></B></a></font>
-        <a href="http://202.137.126.58/Logout"><font color="WHITE " size="4"><B>LOGOUT</B></a></font>
+        <a href="#"><font color="WHITE " size="2"><B><?=$_SESSION['username']?></B></a></font>
+        <a href="http://202.137.126.58/Logout"><font color="WHITE " size="2"><B>LOGOUT</B></a></font>
        
     <!-- <a href="chat_admin.php"> <font color="White" size="4"><B>Chat Room</a></B></font>
 	  		<a href="ajax.php?action=logout"><font color="White" size="4"><B><?php echo $_SESSION['username'] ?></B></font> <i class="fa fa-power-off"></i></a>
