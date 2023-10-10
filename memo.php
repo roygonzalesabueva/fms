6b6fc2acd6a8c331069c82ec9449e029599e76c8
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-10 18:59:36 +0800

    Update memo.php

--- memo.php
+++ memo.php
@@ -98,7 +98,7 @@ if(isset($_POST['search']))
        
     <title>Division of Davao del Sur|M.E.M.O</title>
 
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+   
 
 
 
@@ -136,50 +136,6 @@ if(isset($_POST['search']))
 
 
   
-
-  <style>
-* {
-  box-sizing: border-box;
-}
-
-body {
-  font-family: Arial, Helvetica, sans-serif;
-}
-
-/* Float four columns side by side */
-.column {
-  float: left;
-  width: 10%;
-  padding: 0 200px;
-}
-
-/* Remove extra left and right margins, due to padding */
-.row {margin: -20 15px;}
-
-/* Clear floats after the columns */
-.row:after {
-  content: "";
-  display: table;
-  clear: both;
-}
-
-/* Responsive columns */
-@media screen and (max-width: 900px) {
-  .column {
-    width: 200%;
-    display: block;
-    margin-bottom: 20px;
-  }
-}
-
-/* Style the counter cards */
-.card {
-  box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2);
-  padding: 16px;
-  text-align: right;
-  background-color: #f1f1f1;
-}
-</style>
 
 
 
