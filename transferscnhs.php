5c8faa178c39078e21f27ff9ddab7fea7a33fb7f
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-16 20:44:37 +0800

    ff

--- transferscnhs.php
+++ transferscnhs.php
@@ -12,6 +12,9 @@
 		$date_updated=$row['date_updated'];
 		
 		
+		mysqli_query($conn, "INSERT INTO `128810` (id,name,date_updated)VALUES('$id','$name', '$date_updated')") or die(mysqli_error());
+	
+		
 		mysqli_query($conn, "INSERT INTO `filesfnhs` (id,name,date_updated)VALUES('$id','$name', '$date_updated')") or die(mysqli_error());
 		
 		mysqli_query($conn, "INSERT INTO `filesmnhs` (id,name,date_updated)VALUES('$id','$name', '$date_updated')") or die(mysqli_error());
