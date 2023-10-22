344e406a1a78c3a523762b01b8b1fae2d4869316
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-22 22:33:58 +0800

    vv

--- memo.php
+++ memo.php
@@ -23,7 +23,7 @@ function myTimer() {
   //if(!isset($_SESSION['login_id']))
     //header('location:login.php');
  include('./header.php'); 
- include('./topbar_public.php'); 
+ include('./topbar2.php'); 
  //include('./auth.php'); 
  ?>
 
