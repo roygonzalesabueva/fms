e1ff0b588d7e652643649cdeb48256fd5c3d24c8
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-26 21:16:06 +0800

    vv

--- memoschool.php
+++ memoschool.php
@@ -379,10 +379,10 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
 ?>
 
 
-<!-- 
+
 
 <center><img src="<?php echo $imageUrl; ?>" alt="Teacher's Picture" class="rounded-circle img-fluid" style="width: 100px;"></center>
-<center><label><?php echo $fname." ".$mname." ".$lname; ?> </label></center> -->
+<center><label><?php echo $fname." ".$mname." ".$lname; ?> </label></center> 
 
 
 
