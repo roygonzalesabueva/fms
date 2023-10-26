6b5e5d9d432ea8f39116bfe0249993d69b0b902b
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-26 14:51:09 +0800

    s

--- chat_index_save.php
+++ chat_index_save.php
@@ -1,25 +1,42 @@
+
 <?php
 session_start();
 
-if(ISSET($_POST['school_id']) && ISSET($_POST['emp_no'])){
-    $_SESSION['school_id'] = $_POST['school_id'];
-    $_SESSION['emp_no'] = $_POST['emp_no'];
-}
+$_SESSION['school_id'] = $schoolid;
+$_SESSION['emp_no'] = $emp_no;
 
-require_once 'conn.php';
+echo "school id" . $schoolid;
+echo "emp ".$emp_no;
 
-if(ISSET($_POST['save'])){
-    $trackid = $_POST['trackid'];
-    $date_created = $_POST['date_created'];
-    $firstname = $_POST['firstname'];
-    $lastname = $_POST['lastname'];
-    $section = $_POST['section'];
-    $address = $_POST['address'];
-
-    // Perform your database operations here
-	mysqli_query($conn, "INSERT INTO `chat` VALUES ('', '$trackid', '$date_created', '$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-    // Redirect back to chat_index.php
-    header("location: chat_index.php?school_id=" . $schoolid . "&emp_no=" . $emp_no);
-
-}
+	require_once'conn.php';
+	
+	if(ISSET($_POST['save'])){
+		
+		$trackid=$_POST['trackid'];
+		$date_created=$_POST['date_created'];
+		$firstname=$_POST['firstname'];
+		$lastname=$_POST['lastname'];
+		$section=$_POST['section'];
+		$address=$_POST['address'];
+		
+		
+		
+		//mysqli_query($conn, "INSERT INTO `membertracking` VALUES ('', '$trackid', '$firstname', '$lastname', '', '$address')") or die(mysqli_error());
+		mysqli_query($conn, "INSERT INTO `chat` VALUES ('', '$trackid', '$date_created', '$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
+		//mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
+		
+		
+	
+		header("location: chat_index.php?school_id=" . $schoolid . "&emp_no=" . $emp_no);
+		
+	}
 ?>
+
+
+
+	
+	
+
+
+
+
