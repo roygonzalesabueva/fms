e3720dad2e9dad511ea8ca8e1a7649016038bd2c
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-10-30 21:02:03 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -413,58 +413,6 @@ function myTimer() {
 
 
 
-                <?php
-require_once('db_tis.php');
-
-// Check if school_id is provided in the GET request
-if (isset($_GET['school_id'], $_GET['emp_no'])) {
-  // Your code to handle both school_id and emp_no
-  $selectedSchoolId = $_GET['school_id'];
-  $selectedEmpNo = $_GET['emp_no'];
- $_SESSION['selSchoolId']=$selectedSchoolId;
- $_SESSION['selEmNo']=$selectedEmpNo;
-
-
-   $sql = "SELECT pi.firstname, pi.lastname, pi.middlename, pi.emp_no, pp.image
-            FROM personal_info AS pi
-            INNER JOIN profile_pic AS pp ON pi.emp_no = pp.emp_no
-            INNER JOIN employment_record AS e ON pp.emp_no = e.emp_no
-            WHERE e.school_id = ? AND pp.emp_no =?";
-
-    if ($stmt = $conn->prepare($sql)) {
-        $stmt->bind_param("ii", $selectedSchoolId, $selectedEmpNo);
-        if ($stmt->execute()) {
-            $result = $stmt->get_result();
-
-            if ($result->num_rows > 0) {
-                while ($row = $result->fetch_assoc()) {
-                    $image = $row['image'];
-                    $imageUrl = "../heroes/admin/$image";
-                    $fname = $row['firstname'];
-                    $lname = $row['lastname'];
-                    $mname = $row['middlename'];
-                    // Output or process $imageUrl as needed
-                }
-            } else {
-                echo "No teachers found for the selected school.";
-            }
-
-            $stmt->close();
-        } else {
-            echo "Error in executing the SQL statement.";
-        }
-    } else {
-        echo "Error in preparing the SQL statement.";
-    }
-
-    // Close the database connection here if needed
-} else {
-    echo "No school_id provided in the GET request.";
-}
-?>
-
-
-
 
 
 
@@ -604,6 +552,82 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
                         
                         
                         <!--	<td><?php echo $fetch['trackid']?></td>-->
+
+
+
+
+
+
+                        <?php
+require_once('db_tis.php');
+
+// Check if school_id is provided in the GET request
+if (isset($_GET['school_id'], $_GET['emp_no'])) {
+  // Your code to handle both school_id and emp_no
+  $selectedSchoolId = $_GET['school_id'];
+  $selectedEmpNo = $_GET['emp_no'];
+ $_SESSION['selSchoolId']=$selectedSchoolId;
+ $_SESSION['selEmNo']=$selectedEmpNo;
+
+
+   $sql = "SELECT pi.firstname, pi.lastname, pi.middlename, pi.emp_no, pp.image
+            FROM personal_info AS pi
+            INNER JOIN profile_pic AS pp ON pi.emp_no = pp.emp_no
+            INNER JOIN employment_record AS e ON pp.emp_no = e.emp_no
+            WHERE e.school_id = ? AND pp.emp_no =?";
+
+    if ($stmt = $conn->prepare($sql)) {
+        $stmt->bind_param("ii", $selectedSchoolId, $selectedEmpNo);
+        if ($stmt->execute()) {
+            $result = $stmt->get_result();
+
+            if ($result->num_rows > 0) {
+                while ($row = $result->fetch_assoc()) {
+                    $image = $row['image'];
+                    $imageUrl = "../heroes/admin/$image";
+                    $fname = $row['firstname'];
+                    $lname = $row['lastname'];
+                    $mname = $row['middlename'];
+                    // Output or process $imageUrl as needed
+                }
+            } else {
+                echo "No teachers found for the selected school.";
+            }
+
+            $stmt->close();
+        } else {
+            echo "Error in executing the SQL statement.";
+        }
+    } else {
+        echo "Error in preparing the SQL statement.";
+    }
+
+    // Close the database connection here if needed
+} else {
+    echo "No school_id provided in the GET request.";
+}
+?>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
                         <td> <img src="<?php echo $imageUrl; ?>" alt="Teacher's Picture" class="rounded-circle img-fluid" style="width: 40px;"></td>
                            
