d6071fcc9c685e2fd16414ccc444b7d62a0ca20c
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-04 20:54:00 +0800

    Update chat_index_save.php

--- chat_index_save.php
+++ chat_index_save.php
@@ -4,7 +4,7 @@
 
 <?php
 require_once('db_tis.php');
-
+require_once 'conn.php';
 // Check if school_id is provided in the GET request
 if (isset($_GET['school_id'], $_GET['emp_no'])) {
   // Your code to handle both school_id and emp_no
@@ -18,6 +18,7 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
             FROM personal_info AS pi
             INNER JOIN profile_pic AS pp ON pi.emp_no = pp.emp_no
             INNER JOIN employment_record AS e ON pp.emp_no = e.emp_no
+            INNER JOIN chat AS c ON pp.emp_no = c.emp_no
             WHERE e.school_id = ? AND pp.emp_no =?";
 
     if ($stmt = $conn->prepare($sql)) {
