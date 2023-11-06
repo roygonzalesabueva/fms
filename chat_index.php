d1c7f2ea78980cee20be4a69f17fe3dd2cf624d0
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-07 04:56:55 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -795,7 +795,9 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
                         <tr>
                            
                             
-                        <td><?php echo $imageUrl?></td>
+                        <td>
+<img src="<?php echo $imageUrl; ?>" alt="Teacher's Picture" class="rounded-circle img-fluid" style="width: 100px;"></center>
+</td>
 
                             <td><?php echo $fetch['image']?></td>
 
