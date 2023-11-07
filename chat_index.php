3390ef5072edb4b3a02d6d01653a0b44c97a87e5
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-07 21:52:48 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -505,7 +505,7 @@ echo "No school_id provided in the GET request.";
 
 <!-- <img src="DAVAOSUR.png" width="140" height="70"> -->
    
-<label>  Project DAVAO<font size="3" color="Orange">SUR</font> <font size="3" color="Blue">Messenger</font></label> 
+<label>  Project <B>DAVAO<font size="3" color="Orange">SUR</font> </B><font size="3" color="Blue">Messenger</font></label> 
 
 
 
