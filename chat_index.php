2cc4019c3682e1379f1e029fb5d3eaf3b1b09938
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-07 21:11:59 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -325,7 +325,7 @@ function myTimer() {
     /* Float four columns side by side */
     .column {
         float: left;
-        width: 90%;
+        width: 120%;
         padding: 0 300px;
     }
 
@@ -344,7 +344,7 @@ function myTimer() {
     /* Responsive columns */
     @media screen and (max-width: 600px) {
         .column {
-            width: 90%;
+            width: 120%;
             display: block;
             margin-bottom: 500px;
         }
@@ -727,7 +727,7 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
                                 <font color="Black"> Message</font></label>
                             -->
 
-                        <input type="text" name="lastname" placeholder="Message.." class="form-control form-control-lg " style="width: 643px;" 
+                        <input type="text" name="lastname" placeholder="Message.." class="form-control form-control-lg " style="width: 500px;" 
                             id="comment"> 
                     </div>
                 </form>
