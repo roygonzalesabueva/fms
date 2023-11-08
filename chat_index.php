94e4788785b1847f01c3227220a70851165b6498
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-08 21:07:18 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -414,10 +414,10 @@ if (isset($_POST['save'])) {
 $trackid = verify($_POST['trackid']);
 $emp_no = verify($_POST['emp_no']);
 $image = verify($_POST['image']);
-$date_created = verify($_POST['date_created']);
+
 $firstname = verify($_POST['firstname']);
 $lastname = verify($_POST['lastname']);
-
+$date_created = verify($_POST['date_created']);
 
 
 
@@ -426,7 +426,7 @@ if (!empty($_SESSION['emp_no']) && !empty($_SESSION['schoolid'])) {
 $emp_no = $_SESSION['emp_no'];
 $school_id = $_SESSION['schoolid'];
 
-mysqli_query($conn, "INSERT INTO `chat` VALUES ('','$image','$emp_no', '$date_created', '$firstname', '$lastname')") or die(mysqli_error());
+mysqli_query($conn, "INSERT INTO `chat` VALUES ('','$emp_no','$image', '$firstname', '$lastname' , '$date_created')") or die(mysqli_error());
 
 header("location: chat_index.php?school_id=" . $school_id . "&emp_no=" . $emp_no);
 } else {
