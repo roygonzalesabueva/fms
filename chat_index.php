efdd5b81ff0754dbbe1bb76c558102ddc0f366a2
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-09 00:56:50 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -534,7 +534,7 @@ echo "No school_id provided in the GET request.";
 
 <!-- <img src="DAVAOSUR.png" width="140" height="70"> -->
    
-<button class="btn"><label> <i class="fa fa-bars"></i>Project DAVAO<font size="3" color="Orange">SUR</font> <font size="3" color="Blue">Messenger</font></label> </button>
+<button class="btn"><label> <i class="fa fa-bars"></i>Project DAVAO<font size="3" color="Orange">SUR</font> <font size="3" color="white">Messenger</font></label> </button>
  
 
 
@@ -556,7 +556,7 @@ echo "No school_id provided in the GET request.";
         
                     <!--  <li><button type="button" class="btn cancel" onclick="closeForm()">Close</button></li>-->   
                   
-                    <button class="btn"><a href="memoschool.php?school_id=<?php echo $schoolid ?>&emp_no=<?php echo $emp_no ?>"> <i class="fa fa-close"></i>  </a></button>
+                    <button class="btn"><a href="memoschool.php?school_id=<?php echo $schoolid ?>&emp_no=<?php echo $emp_no ?>"> <font color="whie"> <i class="fa fa-close"></i> </font> </a></button>
                       <!-- <input type="submit" class="btn btn-block btn-primary btn-lg font-weight-medium auth-form-btn" name ="login1"  value="Close" />   -->
                 
                      
