2857669a77ea0b809f8e3079682a809407bfc3b8
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-08 21:39:02 +0800

    kk

--- chat_index.php
+++ chat_index.php
@@ -453,7 +453,6 @@ $sql = "SELECT pi.firstname, pi.lastname, pi.middlename, pi.emp_no, pp.image
 FROM personal_info AS pi
 INNER JOIN profile_pic AS pp ON pi.emp_no = pp.emp_no
 INNER JOIN employment_record AS e ON pp.emp_no = e.emp_no
-
 WHERE e.school_id = ? AND pp.emp_no =?"; 
 
 if ($stmt = $conn->prepare($sql)) {
@@ -620,7 +619,6 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
             FROM personal_info AS pi
             INNER JOIN profile_pic AS pp ON pi.emp_no = pp.emp_no
             INNER JOIN employment_record AS e ON pp.emp_no = e.emp_no
-           
             WHERE e.school_id = ? AND pp.emp_no =?";
 
     if ($stmt = $conn->prepare($sql)) {
--- chat_index_save.php
+++ chat_index_save.php
@@ -18,7 +18,6 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
             FROM personal_info AS pi
             INNER JOIN profile_pic AS pp ON pi.emp_no = pp.emp_no
             INNER JOIN employment_record AS e ON pp.emp_no = e.emp_no
-            
             WHERE e.school_id = ? AND pp.emp_no =?";
 
     if ($stmt = $conn->prepare($sql)) {
