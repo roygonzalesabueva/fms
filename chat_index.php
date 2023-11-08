ac143b44fa2736e11a70d82c1a761dbf3d42120b
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-08 22:04:19 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -785,7 +785,7 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
 
                         <?php while($fetch = mysqli_fetch_array($search_result)): ?>
                         <tr>
-                        <td><?php echo $fetch['emp_no']?></td>
+                        <td><?php echo $fetch['pi']?></td>
                            
                         
                         <!-- <td><?php echo $fetch['image']?></td> -->
