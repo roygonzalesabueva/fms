4ea1c6a041442d01b0f556edc3a76fa445ceffb4
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-08 20:36:39 +0800

    Update chat_index.php

--- chat_index.php
+++ chat_index.php
@@ -673,9 +673,10 @@ if (isset($_GET['school_id'], $_GET['emp_no'])) {
                         <select type="text" name="image" placeholder="" class="form-control" required="required"
                             readonly />
 
-                            <option value=" <?php echo $row['image']; ?> "> <?php echo $row['image']; ?></option>
+                            <!-- <option value=" <?php echo $row['image']; ?> "> <?php echo $row['image']; ?></option> -->
                            
-                            
+                            <option value="<?php echo $_SESSION['image'];?>"><?php echo $_SESSION['image'];?></option>
+                    
 
                             <!-- <option value="img src="<?php echo $row['image']; ?> alt="Teacher's Picture" class="rounded-circle img-fluid" style="width: 40px;">
 "> <img src="<?php echo $row['image']; ?>" alt="Teacher's Picture" class="rounded-circle img-fluid" style="width: 40px;">
