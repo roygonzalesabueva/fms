973a6f87a9941bb13eab44a0e3f4a35ee44f43bb
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-10 21:02:02 +0800

    Update topbar_davsur.php

--- topbar_davsur.php
+++ topbar_davsur.php
@@ -56,8 +56,8 @@
 
   			
         <font color="White" size="3"><B><a <p id="demo"></p></a> | </B></font> 
-        <!-- <a href="chat_davsur.php"><font color="White" size="3"><B>Chat Room</a> | </B></font> -->
-        <a href="chat_davsur.php?school_id=<?php echo $schoolid ?>&emp_no=<?php echo $emp_no ?>"><font color="White" size="3"><B>Chat Room</a> | </B></font>
+        <a href="chat_davsur.php"><font color="White" size="3"><B>Chat Room</a> | </B></font>
+        <!-- <a href="chat_davsur.php?school_id=<?php echo $schoolid ?>&emp_no=<?php echo $emp_no ?>"><font color="White" size="3"><B>Chat Room</a> | </B></font> -->
   	
 
         <a href="#"><font color="WHITE " size="3"><i class="fa fa-fw fa-user"></i><B><?=$_SESSION['username']?></a> | </B></font>
