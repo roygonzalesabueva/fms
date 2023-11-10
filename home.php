6251c32bcd6650c03eee10ef9456ff405140724a
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-10 20:39:27 +0800

    Update home.php

--- home.php
+++ home.php
@@ -339,8 +339,8 @@ body {
 
       <div class="row">
            
-<!-- <form action="home.php" method="post" enctype="multipart/form-data"> -->
-<form action="home.php?school_id=<?=$_SESSION['selSchoolId']?>&emp_no=<?=$_SESSION['selEmNo']?>" method="post" enctype="multipart/form-data">
+<form action="home.php" method="post" enctype="multipart/form-data">
+<!-- <form action="home.php?school_id=<?=$_SESSION['selSchoolId']?>&emp_no=<?=$_SESSION['selEmNo']?>" method="post" enctype="multipart/form-data"> -->
  
                 
                 <input type="file" name="myfile" >  
