21b83930b72caf7221edf64a4678b99b700a091e
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-14 02:11:19 +0800

    ccc

--- home.php
+++ home.php
@@ -363,8 +363,9 @@ body {
 <!-- <form action="home.php" method="post" enctype="multipart/form-data"> -->
 <!-- <form action="home.php?school_id=<?=$_SESSION['selSchoolId']?>&emp_no=<?=$_SESSION['selEmNo']?>" method="post" enctype="multipart/form-data"> -->
            
-<form action="home.php?school_id=<?=$_SESSION['selSchoolId']?>&emp_no=<?=$_SESSION['selEmNo']?>" method="post" enctype="multipart/form-data">
-   
+<form action="home.php" method="post" enctype="multipart/form-data">
+<!-- <form action="home.php?school_id=<?=$_SESSION['selSchoolId']?>&emp_no=<?=$_SESSION['selEmNo']?>" method="post" enctype="multipart/form-data"> -->
+
                 
                 <input type="file" name="myfile" >  
                 
