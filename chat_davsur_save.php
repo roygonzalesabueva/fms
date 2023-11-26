d8f3a623e0f116f60a6b8b1d365b98f8af9b37e0
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-11-26 21:41:49 +0800

    vv

--- chat_davsur_save.php
+++ chat_davsur_save.php
@@ -1,95 +1,23 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 <?php
-session_start();
-
-if (!isset($_SESSION['username'])) {
-    header("Location: http://202.137.126.58/");
-    exit();
-}
-
-function verify($data){
-    $data = trim($data);
-    $data = htmlspecialchars($data);
-    $data = stripslashes($data);
-    return $data;
-}
-
-if (isset($_POST['login1'])) {
-    // Getting the form data
-    // ... your existing login code ...
-
-    // Ensure that $_SESSION['emp_no'] and $_SESSION['schoolid'] are properly set.
-
-    if (!empty($_SESSION['emp_no']) && !empty($_SESSION['schoolid']) && !empty($_SESSION['image'])) {
-        // Your login code successfully set emp_no and schoolid in the session.
-    } else {
-        // Handle the case where emp_no or schoolid is not set.
-        echo "Emp_no and/or schoolid is not set in the session.";
-    }
-}
-
-
-
-
-
-
-
-
-
-require_once 'conn.php';
-
-if (isset($_POST['save'])) {
-    
-    $emp_no = verify($_POST['emp_no']);
-    // $image = verify($_POST['image']);
-    $date_created = verify($_POST['date_created']);
-    $firstname = verify($_POST['firstname']);
-    $lastname = verify($_POST['lastname']);
-    
-    // $section = verify($_POST['section']);
-    // $address = verify($_POST['address']);
-
-
-
-
-
-
-
-    // Ensure emp_no and schoolid are available in the session.
-    if (!empty($_SESSION['emp_no']) && !empty($_SESSION['schoolid'])) {
-        $emp_no = $_SESSION['emp_no'];
-        
-        $school_id = $_SESSION['schoolid'];
-        $image = $_SESSION['image'];
-        
-        mysqli_query($conn, "INSERT INTO `chat` VALUES ('','$emp_no','$image', '$date_created', '$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-
-        header("location: chat_davsur.php");
-    } else {
-        header("location: chat_davsur.php");
-    }
-}
-
-
-
-
-
-
-
+	require_once'conn.php';
+	
+	if(ISSET($_POST['save'])){
+		
+		$trackid=$_POST['trackid'];
+		$date_created=$_POST['date_created'];
+		$firstname=$_POST['firstname'];
+		$lastname=$_POST['lastname'];
+		$section=$_POST['section'];
+		$address=$_POST['address'];
+		
+		
+		
+		//mysqli_query($conn, "INSERT INTO `membertracking` VALUES ('', '$trackid', '$firstname', '$lastname', '', '$address')") or die(mysqli_error());
+		mysqli_query($conn, "INSERT INTO `chat` VALUES ('', '$trackid', '$date_created', '$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
+		//mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
+		
+	
+		header("location: chat_davsur.php");
+		
+	}
 ?>
