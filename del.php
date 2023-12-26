5cad01340caa77899130926aa3f9fca574ada95e
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-12-26 17:04:32 +0800

    Update del.php

--- del.php
+++ del.php
@@ -1,3 +1,5 @@
+
+
 <?php
 	require_once'db_connect.php';
 	
@@ -5,22 +7,33 @@
 		$id=$_REQUEST['id'];
 		
 		$query=mysqli_query($conn, "SELECT * FROM `files` WHERE `id`='$id'") or die(mysqli_error());
-		$query=mysqli_query($conn, "SELECT * FROM `filesscnhs` WHERE `id`='$id'") or die(mysqli_error());
 		$row=mysqli_fetch_array($query);
 		
 		$id=$row['id'];
 		$name=$row['name'];
 		$date_updated=$row['date_updated'];
+
+
+ 
+ 
+ 
+	 mysqli_query($conn, "INSERT INTO `stored` (id,name,date_updated)VALUES('$id','$name', '$date_updated')") or die(mysqli_error());
 		
+		// mysqli_query($conn, "INSERT INTO `filesscces` (id,name,date_updated)VALUES('$id','$name', '$date_updated')") or die(mysqli_error());
+			
+		// mysqli_query($conn, "INSERT INTO `filespnhs` (id,name,date_updated)VALUES('$id','$name', '$date_updated')") or die(mysqli_error());
 		
+		//mysqli_query($conn, "INSERT INTO `filesrecords` (id,name,date_updated)VALUES('$id','$name', '$date_updated')") or die(mysqli_error());
 		
-		//mysqli_query($conn, "INSERT INTO `membertracking` VALUES('','$trackid','$firstname', '$lastname', '', '$address')") or die(mysqli_error());
-	//	mysqli_query($conn, "INSERT INTO `membersds` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-		//mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
-		mysqli_query($conn, "DELETE FROM `files` WHERE `id`='$id'") or die(mysqli_error());
-		mysqli_query($conn, "DELETE FROM `filesscnhs` WHERE `id`='$id'") or die(mysqli_error());
-		
-		// echo"<script>alert('Files successfully deleted')</script>";
+		//mysqli_query($conn, "DELETE FROM `files` WHERE `id`='$id'") or die(mysqli_error());
+
+
+
+		echo"<script>alert('Memorandum successfully deleted')</script>";
 		echo"<script>window.location='index.php?page=files2'</script>";
+		
 	}
+
+
+
 ?>
