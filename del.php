d1c2d715c2ad21759e805f197a35f59b3849085f
roygonzalesabueva <roy.abueva@deped.gov.ph>
2023-12-26 16:17:43 +0800

    e

--- del.php
+++ del.php
@@ -5,6 +5,7 @@
 		$id=$_REQUEST['id'];
 		
 		$query=mysqli_query($conn, "SELECT * FROM `files` WHERE `id`='$id'") or die(mysqli_error());
+		$query=mysqli_query($conn, "SELECT * FROM `filesscnhs` WHERE `id`='$id'") or die(mysqli_error());
 		$row=mysqli_fetch_array($query);
 		
 		$id=$row['id'];
@@ -17,6 +18,7 @@
 	//	mysqli_query($conn, "INSERT INTO `membersds` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
 		//mysqli_query($conn, "INSERT INTO `memberclient` (mem_id,trackid,firstname,lastname,section,address) VALUES('','$trackid','$firstname', '$lastname', '$section','$address')") or die(mysqli_error());
 		mysqli_query($conn, "DELETE FROM `files` WHERE `id`='$id'") or die(mysqli_error());
+		mysqli_query($conn, "DELETE FROM `filesscnhs` WHERE `id`='$id'") or die(mysqli_error());
 		
 		// echo"<script>alert('Files successfully deleted')</script>";
 		echo"<script>window.location='index.php?page=files2'</script>";
