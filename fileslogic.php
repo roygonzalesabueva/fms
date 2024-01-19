c4bbbeff7884b09cea880655cc951524a1a32789
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-19 19:08:19 +0800

    Update fileslogic.php

--- fileslogic.php
+++ fileslogic.php
@@ -1,112 +1,60 @@
 
+<?php
 
+$conn = mysqli_connect('localhost', 'root', '@DavaosurDB2023', 'fms_db');
 
-<?php 
-
-$conn=mysqli_connect('localhost','root','@DavaosurDB2023','fms_db');
-
-//or die("Could not connect to mysql".mysqli_error($con));
-
-
-//download
+// Download
 
 $sql = "SELECT * FROM files";
-$result = mysqli_query($conn,$sql);
-$files = mysqli_fetch_all($result,MYSQLI_ASSOC);
+$result = mysqli_query($conn, $sql);
+$files = mysqli_fetch_all($result, MYSQLI_ASSOC);
 
+// End download
 
-//end download
+if (isset($_POST['save'])) {
+    $filename = $_FILES['myfile']['name'];
+    $destination = 'assets/uploads/' . $filename;
 
+    $file = $_FILES['myfile']['tmp_name'];
 
+    if (move_uploaded_file($file, $destination)) {
+        $sql = "INSERT INTO files (name) VALUES ('$filename')";
 
-if(isset($_POST['save']))
-{
-	$filename = $_FILES['myfile']['name'];
-	$destination ='assets\uploads/'. $filename;
-
-	//$extension = pathinfo($filename,PATHINFO_EXTENSION);
-
-	$file = $_FILES['myfile']['tmp_name'];
-
-	
-
-	/*if(!in_array($extension,['zip','pdf','png']))
-	{
-
-		echo "Your file extension must be .zip .pdf .png .jpeg";
-	}
-
-	
-	else{*/
-
-		if(move_uploaded_file($file,$destination))
-		{
-			$sql="INSERT INTO files (name)
-			VALUES('$filename')";
-
-			if(mysqli_query($conn,$sql))
-			{
-				
-				
-		
-
-				
-			}
-			else{
-				echo "failed to upload file";
-			}
-		}
-/*	}*/
-
+        if (mysqli_query($conn, $sql)) {
+            // File uploaded successfully
+        } else {
+            echo "Failed to upload file";
+        }
+    }
 }
 
+// Download code - last part entry get data
 
-//download code last part entry get data
+if (isset($_GET['file_id'])) {
+    $id = $_GET['file_id'];
+    $sql = "SELECT * FROM files WHERE id=$id";
+    $result = mysqli_query($conn, $sql);
+    $file = mysqli_fetch_assoc($result);
 
-if(isset($_GET['file_id']))
-{
+    $filepath = 'assets/uploads/' . $file['name'];
 
-	$id = $_GET['file_id'];
-	$sql="SELECT * FROM files WHERE id=$id";
+    if (file_exists($filepath)) {
+        header('Content-Type: ' . mime_content_type($filepath));
+        header('Content-Description: File Transfer');
+        header('Content-Disposition: attachment; filename=' . basename($filepath));
+        header('Expires: 0');
+        header('Cache-Control: must-revalidate');
+        header('Pragma: public');
+        header('Content-Length: ' . filesize($filepath));
+        readfile($filepath);
 
-	$result = mysqli_query($conn,$sql);
+        $newCount = $file['downloads'] + 1;
+        $updateQuery = "UPDATE files SET downloads=$newCount WHERE id=$id";
+        mysqli_query($conn, $updateQuery);
 
-	$file = mysqli_fetch_assoc($result);
-
-	$filepath ='assets\uploads/' . $file['name'];
-
-
-	if(file_exists($filepath))
-	{
-		header('Content-Type: application/octet-stream');
-
-		header('Content-Description: File Transfer');
-		header('Content-Disposition: attachment; filename=' . basename($filepath));
-		header('Expires: 0');
-		header('Cache-Control: must-revalidate');
-		header('Pragma:public');
-		header('Content-Length:' . filesize('assets\uploads/'.$file['name']));
-		readfile('assets\uploads/' . $file['name']);
-
-		$newCount = $file['downloads'] + 1;
-
-		$updatQuery = "UPDATE files SET downloads=$newCount WHERE id=$id";
-
-		mysqli_query($conn,$updatQuery);
-		exit;
-
-
-	}
-
-
+        exit;
+    }
 }
 
-
-
-//end
-
-
-
-
-
+// End
 ?>
