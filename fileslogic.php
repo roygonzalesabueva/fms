6931ebc668343155955bfc5763c60731e93037e0
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-01-19 16:01:23 +0800

    Update fileslogic.php

--- fileslogic.php
+++ fileslogic.php
@@ -30,10 +30,10 @@ if(isset($_POST['save']))
 
 	
 
-	if(!in_array($extension,['zip','pdf','png','JPG']))
+	if(!in_array($extension,['zip','pdf','png','jpg']))
 	{
 
-		echo "Your file extension must be .zip .pdf .png .JPG";
+		echo "Your file extension must be .zip .pdf .png .jpg";
 	}
 
 	
