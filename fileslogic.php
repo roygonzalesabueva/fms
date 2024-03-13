78814dba3519e7341f21a84b369116d9ba5fa3d5
roygonzalesabueva <roy.abueva@deped.gov.ph>
2024-03-13 11:44:04 +0800

    Update fileslogic.php

--- fileslogic.php
+++ fileslogic.php
@@ -1,8 +1,3 @@
-
-
-
-
-
 <?php
 
 $conn = mysqli_connect('localhost', 'root', '@DavaosurDB2023', 'fms_db');
@@ -42,8 +37,7 @@ if (isset($_GET['file_id'])) {
 
     $filepath = 'assets/uploads/' . $file['name'];
 
-    // Check if the file exists and is one of the allowed file types (JPEG, PDF, PNG)
-    if (file_exists($filepath) && (exif_imagetype($filepath) === IMAGETYPE_JPEG || strtolower(pathinfo($filepath, PATHINFO_EXTENSION)) === 'pdf' || exif_imagetype($filepath) === IMAGETYPE_PNG)) {
+    if (file_exists($filepath)) {
         $finfo = finfo_open(FILEINFO_MIME_TYPE);
         $mime = finfo_file($finfo, $filepath);
         finfo_close($finfo);
@@ -62,13 +56,17 @@ if (isset($_GET['file_id'])) {
         mysqli_query($conn, $updateQuery);
 
         exit;
-    } else {
-        echo "File not found or not one of the allowed file types (JPEG, PDF, PNG).";
     }
 }
 
 // End
 ?>
+
+
+
+
+
+
 
 
 
